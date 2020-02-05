2b8105eed7c759e1a481916183ba7c361088fe0d
Fatma Dogan and Kristina Hengster Tintor <kristina@>
2020-02-05 09:43:18 -0800

    run first test and fail

--- __test__/weekday.test.js
+++ __test__/weekday.test.js
@@ -4,8 +4,8 @@ import { exportAllDeclaration } from '@babel/types';
 describe ('Weekday', () => {
   
   test('should correctly create a weekday object with date', () => {
-    var weekday = new Weekday(1-1-2020);
-    expect(weekday.date1).toEqual(1-1-2020);
+    var weekday = new Weekday("1-1-2020");
+    expect(weekday.date1).toEqual("1-1-2020");
   });
 
 });
