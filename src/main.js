a7ada7037f0e145d8aa335abadd1a5d400076421
Student <example@example.com>
2020-02-06 14:11:03 -0800

    fix bugs

--- src/main.js
+++ src/main.js
@@ -2,22 +2,47 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Weekday } from './weekday';
 import { UserDate } from './weekday';
-import { convertDayName} from './weekday';
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var inputtedDay = $("input#day").val();
-    var inputtedMonth = $("input#month").val();
-    var inputtedYear = $("input#year").val();
+    let inputtedDay = $("input#day").val();
+    let inputtedMonth = $("input#month").val();
+    let inputtedYear = $("input#year").val();
 
-    var inputtedUserDate = new UserDate (inputtedDay, inputtedMonth, inputtedYear);
+    let inputtedUserDate = new UserDate (inputtedDay, inputtedMonth, inputtedYear);
+   
     console.log(inputtedUserDate);
 
+    let array = ([inputtedMonth, inputtedDay, inputtedYear]).toString();
+    console.log(array);
+    var d = new Date(array);
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  
+   let type = inputtedUserDate.checkType();
+
+  //  let array2 = ([inputtedMonth, inputtedYear]).toString();
+   let y = new Date(inputtedYear, inputtedMonth, 0);
+
+   let daysInMonth = y.getDate();
+ 
+
+  //  let getDaysInMonth = function(inputtedMonth, inputtedYear){
+  //   new Date(inputtedYear, inputtedMonth, 0).getDate()
+  //   };
+    console.log(daysInMonth);
+    console.log(inputtedMonth);
+
+    if(!type || inputtedDay > daysInMonth){
+      $("#output").text("Date is not valid");
+    } else{
+      $("#output").text(days[d.getDay()]);
+    }
+  
+    
 
 
-
+    // console.log(d.getDay());
   });
 });
